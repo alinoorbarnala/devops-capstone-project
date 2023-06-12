7b8c2d890a2f0683feac4750296802e5031a656f
alinoorbarnala <alinoorbarnala@gmail.com>
2023-06-12 10:10:42 -0400

    completed ci build

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -161,14 +161,25 @@ class TestAccountService(TestCase):
 
     def test_update_account(self):
         """--My Test-- It Should update account"""
-        account= self._create_accounts(1)[0]
-        account.name="Ali"
-        resp = self.client.put(
-            f"{BASE_URL}/{account.id}", content_type="application/json"
-        )
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+        
+        # update the account
+        new_account = resp.get_json()
+        new_account["name"] = "Ali"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Ali")
+
 
     def test_update_account_not_found(self):
         """ --My Test-- It should not update an Account that is not found"""
         resp = self.client.put(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)     
+
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
