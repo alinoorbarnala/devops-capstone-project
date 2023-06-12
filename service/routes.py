8a1daa9f5e01351920801cc28d57662afcb7346b
alinoorbarnala <136201065+alinoorbarnala@users.noreply.github.com>
2023-06-12 20:13:29 +0500

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -84,7 +84,6 @@ def get_accounts(account_id):
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
-    
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
 
@@ -102,14 +101,11 @@ def update_accounts(account_id):
     This endpoint will update an Account based the account_id that is requested
     """
     app.logger.info("Request to update an Account with id: %s", account_id)
- 
     account = Account.find(account_id)
-
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     account.deserialize(request.get_json())
     account.update()
-    
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -124,9 +120,9 @@ def delete_accounts(account_id):
     This endpoint will delete an Account based the account_id that is requested
     """
     app.logger.info("Request to delete an Account with id: %s", account_id)
-    account = Account.find(account_id) 
+    account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")  
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     Account.delete(account)
     return "", status.HTTP_204_NO_CONTENT
 
